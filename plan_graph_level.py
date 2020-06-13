5d59b47bd7f693c080469a23c0d802bf16e19fb8
michal balaban <balabanmich@gmail.com>
2020-06-13 16:37:04 +0300

    Fixed problem

--- plan_graph_level.py
+++ plan_graph_level.py
@@ -213,8 +213,8 @@ def have_competing_needs(a1, a2, mutex_props):
     for pre_a1 in a1.get_pre():
         for pre_a2 in a2.get_pre():
             if Pair(pre_a1, pre_a2) in mutex_props:
-                return False
-    return True
+                return True
+    return False
 
 
 def mutex_propositions(prop1, prop2, mutex_actions_list):
